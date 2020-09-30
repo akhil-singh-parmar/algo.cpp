93415906ff61837deba8760d9c1ea09d3d0af42d
Maran1947 <69248165+Maran1947@users.noreply.github.com>
2020-10-01 01:37:27 +0530

    Bubble Sort

---
+++ Bubble_sort.java
@@ -0,0 +1,25 @@
+package Algo;
+
+public class Bubble_sort {
+
+	public static void main(String[] args) {
+		int arr[] = {121, 101, 51, 21, 151};
+		int temp = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < (arr.length - i - 1); j++) {
+				if(arr[j] > arr[j + 1]) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+}
