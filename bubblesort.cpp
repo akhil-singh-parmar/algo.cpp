e5d23be121255ca59dd8b7dad548b5a47120ed40
its-akhilsingh <72935888+its-akhilsingh@users.noreply.github.com>
2020-10-17 13:00:30 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -18,7 +18,7 @@ void bubblesort(int arr[],int n)
 }
 void printarray(int arr[],int n)  
   {
-  cout<<"the sorted array is ";
+  cout<<"the elements of this array are ";
   for(int k=0;k<n;k++)
   cout<<arr[k]<<" ";
 }
